14020cdcc9ba26389fdfac3449106d770b262657
Jinshi <luxxx917@umn.edu>
2018-10-10 06:16:35 -0500

    fix

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def api_results():
     with db.get_db_cursor(commit=True) as cur:
         reverse = request.args.get('reverse')
         if reverse is None:
-            cur.execute("SELECT * FROM survey_data ORDER BY ts DESC")
+            cur.execute("SELECT * FROM survey_data ORDER BY survey_id DESC")
         else:
             cur.execute("SELECT * FROM survey_data")
         rec = cur.fetchall()
